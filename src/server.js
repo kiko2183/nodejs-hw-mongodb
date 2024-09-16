3b5d47fe2ad2d970820fbe63760a643f2b2779a4
kiko2183 <127057180+kiko2183@users.noreply.github.com>
2024-09-16 15:17:05 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,61 +1,59 @@
-import express from 'express';
-import cors from 'cors';
-import pino from 'pino-http';
-import {env} from './utils/env.js'
-import * as contactServices from './services/contact.js'
+import express from "express";
+import cors from "cors";
 
-export const setupServer = () => {
-const app = express();
+import { env } from "./utils/env.js";
 
-const logger = pino({
-    transport: {
-        target: "pino-pretty"
-    }
-});
+import * as contactServices from "./services/contacts.js";
 
-app.use(logger);
-app.use(cors());
-app.use(express.json());
+export const startServer = ()=>{
+    const app = express();
 
-app.get("/contacts", async (req, res) => {
-    const data = await contactServices.getAllContacts();
+    app.use(cors());
+    app.use(express.json());
 
-    res.json({
-        status: 200,
-        message: "Successfully found contacts!",
-        data,
-    });
-});
+    app.get("/contacts", async (req, res)=> {
+        const data = await contactServices.getAllContacts();
+ 
+         res.json({
+             status: 200,
+             message: "Successfully found contacts!",
+             data,
+         });
+     });
+ 
+     app.get("/contacts/:ContactId", async(req, res)=> {
+         const {ContactId} = req.params;
+         const data = await contactServices.getContactById(ContactId);
+ 
+         if(!data) {
+             return res.status(404).json({
+                 message: `Contact with id=${ContactId} not found`
+             });
+         }
+ 
+         res.json({
+             status: 200,
+             message: `Contact with ${ContactId} successfully find`,
+             data,
+         });
+     });
+ 
+     app.use((req, res)=> {
+         res.status(404).json({
+             message: `${req.url} not found`
+         });
+     });
+ 
+     app.use((error, req, res, next)=> {
+         res.status(500).json({
+             message: error.message,
+         });
+     });
+ 
 
-app.get("/contacts/:id", async(req, res) => {
-   const {id} = req.params;
-   const data = await contactServices.getContactById(id);
+    const port = Number(env("PORT", 3000));
 
-   if(!data){
-   return res.status(404).json({
-        message: "Contact not found",
-    });
-   };
-
-    res.json({
-        status: 200,
-        message:`Successfully found contact with id {id}`,
-        data,
-    });
-
-});
-
-app.use((req, res) => {
-    res.status(404).json({
-        message: `${req.url} not found`,
-    });
-});
-
-app.use((error, req, res, next) => {
-    res.status(500).json({
-        message: error.message,
-    });
-});
-const port = Number(env("PORT", 3000));
-app.listen(port, () => console.log(`Server running on port ${port}`));
+    app.listen(port, ()=> console.log("Server running on port 3000"));
 };
+
+// changes
