37ca433f0951c86e02ca61d3164ec30f79531bb2
kiko2183 <romankulicnij@gmail.com>
2024-09-26 16:36:01 +0300

    1

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import { env } from "./utils/env.js";
 import notFoundHandler from "./middlewares/notFoundHandler.js";
 import errorHandler from "./middlewares/errorHandler.js";
-import logger from "./middlewares/loger.js";
+import logger from "./middlewares/logger.js";
 
 import contactsRouter from "./routers/contacts.js";
 
