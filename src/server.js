87dfb4db0e0a1df1dc52fb55d920d0221b72d152
kiko2183 <romankulicnij@gmail.com>
2024-09-26 16:47:03 +0300

    1

--- src/server.js
+++ src/server.js
@@ -1,23 +1,22 @@
-import express from "express";
-import cors from "cors";
-import { env } from "./utils/env.js";
-import notFoundHandler from "./middlewares/notFoundHandler.js";
-import errorHandler from "./middlewares/errorHandler.js";
-import logger from "./middlewares/logger.js";
+import express from 'express';
+import cors from 'cors';
+import {env} from './utils/env.js'
+import contactsRouter from './routes/contacts.js';
+import notFoundHandler from './middlewares/notFoundHandler.js';
+import errorHandler from './middlewares/errorHandler.js';
+import logger from './middlewares/logger.js';
 
-import contactsRouter from "./routers/contacts.js";
+export const setupServer = () => {
 
-export const startServer = ()=> {
-    const app = express();
+const app = express();
 
-    app.use(logger);
-    app.use(cors());
-    app.use(express.json());
-    app.use("/contacts", contactsRouter);
-    app.use(notFoundHandler);
-    app.use(errorHandler);
+app.use(logger);
+app.use(cors());
+app.use(express.json());
+app.use("/contacts", contactsRouter);
+app.use(notFoundHandler);
+app.use(errorHandler);
 
-    const port = Number(env("PORT", 3000));
-
-    app.listen(port, ()=> console.log("Server running on port 3000"));
+const port = Number(env("PORT", 3000));
+app.listen(port, () => console.log(`Server running on port ${port}`));
 };
