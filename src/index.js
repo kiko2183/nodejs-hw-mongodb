d627e84b761904ba78a5d6ba52e437c7f1236504
kiko2183 <romankulicnij@gmail.com>
2024-09-26 16:58:39 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import {startServer} from "./server.js";
+import { setupServer } from "./server.js";
 import { initMongoConnection } from "./db/initMongoConnection.js";
 
-const bootstrap = async()=> {
-    await initMongoConnection();
-    startServer();
+const bootstrap = async () => {
+  await initMongoConnection();
+  setupServer();
 };
 
 bootstrap();
