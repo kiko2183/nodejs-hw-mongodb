12d39ac9e631ff749c544ae847e5bad1c4b20c66
kiko2183 <romankulicnij@gmail.com>
2024-10-05 10:25:04 +0300

    1

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -3,19 +3,17 @@ import Joi from "joi";
 import { contactTypeList } from "../constants/contacts.js";
 
 export const contactAddSchema = Joi.object({
-    name: Joi.string().required().min(3).max(20),
-    phoneNumber: Joi.string().required().min(3).max(20).messages({
-        "any.required": "number must be exist",
-    }),
-    contactType: Joi.string().required().valid(...contactTypeList).default('personal').min(3).max(20),
-    isFavourite: Joi.boolean(),
-    
+	name: Joi.string().required().min(3).max(20),
+	phoneNumber: Joi.string().required().min(3).max(20),
+	email: Joi.string().min(3).max(20),
+	isFavourite: Joi.boolean(),
+	contactType: Joi.string().required().valid(...contactTypeList).default('personal').min(3).max(20)
 });
 
 export const contactPatchSchema = Joi.object({
-    name: Joi.string().min(3).max(20),
-    email: Joi.string().email().required(),
+	name: Joi.string().min(3).max(20),
 	phoneNumber: Joi.string().min(3).max(20),
+	email: Joi.string().min(3).max(20),
 	isFavourite: Joi.boolean(),
 	contactType: Joi.string().valid(...contactTypeList).default('personal').min(3).max(20)
 });
