665ac154f87affd43342d98047b805d640187787
kiko2183 <romankulicnij@gmail.com>
2024-10-09 01:19:23 +0300

    1

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -5,96 +5,120 @@ import * as contactServices from '../services/contacts.js';
 import { parseContactsFilterParams } from '../utils/filters/parseContactsFilterParams.js';
 import { sortFields } from '../db/Contacts.js';
 
-export const getAllContactsController = async (req, res) => {
-  const { perPage, page } = parsePaginationParams(req.query);
-  const { sortBy, sortOrder } = parseSortParams({ ...req.query, sortFields });
-  const filter = parseContactsFilterParams(req.query);
-  const { _id: userId } = req.user;
+export const getAllContactsController = async (req, res, next) => {
+  try {
+    const { perPage, page } = parsePaginationParams(req.query);
+    const { sortBy, sortOrder } = parseSortParams({ ...req.query, sortFields });
+    const filter = parseContactsFilterParams(req.query);
+    const { _id: userId } = req.user;
 
-  const data = await contactServices.getContacts({
-    perPage,
-    page,
-    sortBy,
-    sortOrder,
-    filter: { ...filter, userId },
-  });
+    const data = await contactServices.getContacts({
+      perPage,
+      page,
+      sortBy,
+      sortOrder,
+      filter: { ...filter, userId },
+    });
 
-  res.json({
-    status: 200,
-    message: 'Successfully found contacts',
-    data,
-  });
-};
-
-export const getContactByIdController = async (req, res) => {
-  const { id } = req.params;
-  const { _id: userId } = req.user;
-  const data = await contactServices.getContact({ _id: id, userId });
-
-  if (!data) {
-    throw createHttpError(404, `Contact with id=${id} not found`);
+    res.json({
+      status: 200,
+      message: 'Successfully found contacts',
+      data,
+    });
+  } catch (error) {
+    next(error);
   }
-
-  res.json({
-    status: 200,
-    message: `Contact with ${id} successfully found`,
-    data,
-  });
 };
 
-export const addContactController = async (req, res) => {
-  const { _id: userId } = req.user;
-  const data = await contactServices.createContact({ ...req.body, userId });
+export const getContactByIdController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { _id: userId } = req.user;
+    const data = await contactServices.getContact({ _id: id, userId });
 
-  res.status(201).json({
-    status: 201,
-    message: 'Contact added successfully',
-    data,
-  });
-};
+    if (!data) {
+      throw createHttpError(404, `Contact with id=${id} not found`);
+    }
 
-export const upsertContactController = async (req, res) => {
-  const { id } = req.params;
-  const { _id: userId } = req.user;
-  const { isNew, data } = await contactServices.updateContact(
-    { _id: id, userId },
-    req.body,
-    { upsert: true }
-  );
-
-  const status = isNew ? 201 : 200;
-
-  res.status(status).json({
-    status,
-    message: 'Contact upserted successfully',
-    data,
-  });
-};
-
-export const patchContactController = async (req, res) => {
-  const { id } = req.params;
-  const { _id: userId } = req.user;
-  const result = await contactServices.updateContact({ _id: id, userId }, req.body);
-
-  if (!result) {
-    throw createHttpError(404, `Contact with id=${id} not found`);
+    res.json({
+      status: 200,
+      message: `Contact with ${id} successfully found`,
+      data,
+    });
+  } catch (error) {
+    next(error); 
   }
-
-  res.json({
-    status: 200,
-    message: 'Contact patched successfully',
-    data: result.data,
-  });
 };
 
-export const deleteContactController = async (req, res) => {
-  const { id } = req.params;
-  const { _id: userId } = req.user;
-  const data = await contactServices.deleteContact({ _id: id, userId });
+export const addContactController = async (req, res, next) => {
+  try {
+    const { _id: userId } = req.user;
+    const data = await contactServices.createContact({ ...req.body, userId });
 
-  if (!data) {
-    throw createHttpError(404, `Contact with id=${id} not found`);
+    res.status(201).json({
+      status: 201,
+      message: 'Contact added successfully',
+      data,
+    });
+  } catch (error) {
+    next(error);
   }
+};
 
-  res.status(204).send();
+export const upsertContactController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { _id: userId } = req.user;
+    const { isNew, data } = await contactServices.updateContact(
+      { _id: id, userId },
+      req.body,
+      { upsert: true }
+    );
+
+    const status = isNew ? 201 : 200;
+
+    res.status(status).json({
+      status,
+      message: 'Contact upserted successfully',
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const patchContactController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { _id: userId } = req.user;
+    const result = await contactServices.updateContact({ _id: id, userId }, req.body);
+
+    if (!result) {
+      throw createHttpError(404, `Contact with id=${id} not found`);
+    }
+
+    res.json({
+      status: 200,
+      message: 'Contact patched successfully',
+      data: result.data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContactController = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { _id: userId } = req.user;
+    const data = await contactServices.deleteContact({ _id: id, userId });
+
+    if (!data) {
+      throw createHttpError(404, `Contact with id=${id} not found`);
+    }
+
+    res.status(204).send();
+  } catch (error) {
+    next(error); 
+  }
 };
--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -1,36 +1,42 @@
 import createHttpError from "http-errors";
 import * as authServices from "../services/auth.js";
 
-const authenticate = async(req, res, next) => {
+const authenticate = async (req, res, next) => {
+    try {
+        const authorization = req.get("Authorization");
+        if (!authorization) {
+            return next(createHttpError(401, "Authorization header not found"));
+        }
 
-    const authorization = req.get("Authorization");
-    if(!authorization){
-        return next(createHttpError(401, "Authorization header not found"))
+        const [bearer, token] = authorization.split(" ");
+        if (!bearer || !token) {
+            return next(createHttpError(401, "Malformed authorization header"));
+        }
+        if (bearer !== "Bearer") {
+            return next(createHttpError(401, "Authorization header must have Bearer type"));
+        }
+
+        const session = await authServices.findSessionByAccessToken(token);
+        if (!session) {
+            return next(createHttpError(401, "Session not found"));
+        }
+
+        if (new Date() > new Date(session.accessTokenValidUntil)) {
+            return next(createHttpError(401, "Access token expired"));
+        }
+
+        const user = await authServices.findUser({ _id: session.userId });
+        if (!user) {
+            return next(createHttpError(401, "User not found"));
+        }
+
+        req.user = user;
+
+        next();
+    } catch (error) {
+        console.error("Error in authentication middleware:", error);
+        next(createHttpError(500, "Internal server error"));
     }
-
-    const [bearer, token] = authorization.split(" ");
-    if(bearer !== "Bearer") {
-        return next(createHttpError(401, "Authorization header must have Bearer type"))
-    }
-
-    const session = await authServices.findSessionByAccessToken(token);
-    if(!session) {
-        return next(createHttpError(401, "Session not found"));
-    }
-
-    if(new Date() > session.accessTokenValidUntil){
-        return next(createHttpError(401, "Access token expired"));
-    }
-
-    const user = await authServices.findUser({_id: session.userId});
-    if(!user){
-        return next(createHttpError(401, "User not found"));
-    }
-
-    req.user = user;
-
-    next();
 };
 
 export default authenticate;
- 
