6216b6a121988c2814b1128a8eaddaf1d788711c
kiko2183 <romankulicnij@gmail.com>
2024-10-10 16:53:47 +0300

    1

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,12 +4,12 @@ import ctrlWrapper from "../utils/ctrlWrapper.js";
 import validateBody from "../utils/validateBody.js";
 import { resetPasswordSchema } from '../validation/users.js';
 import { resetPasswordController } from '../controllers/auth.js';
-import {userSignupSchema, userSigninSchema, requestResetEmailSchema} from "../validation/users.js";
+import {userSignupSchema, userLoginSchema, requestResetEmailSchema} from "../validation/users.js";
 
 const authRouter = Router();
 
 authRouter.post("/register", validateBody(userSignupSchema), ctrlWrapper(authControllers.registerController));
-authRouter.post("/login", validateBody(userSigninSchema), ctrlWrapper(authControllers.loginController));
+authRouter.post("/login", validateBody(userLoginSchema), ctrlWrapper(authControllers.loginController));
 authRouter.post("/refresh", ctrlWrapper(authControllers.refreshController));
 authRouter.post("/logout", ctrlWrapper(authControllers.logoutController));
 authRouter.post("/send-reset-email", validateBody(requestResetEmailSchema), ctrlWrapper(authControllers.requestResetEmailController));
