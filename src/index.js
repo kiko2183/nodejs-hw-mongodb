8059383ca06321869914a67e6aa8bcb068b2f781
kiko2183 <romankulicnij@gmail.com>
2024-10-10 16:38:26 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { setupServer } from "./server.js";
 import { initMongoConnection } from "./db/initMongoConnection.js";
 import createDirIfNotExists from "./utils/createDirIfNotExists.js";
-import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from "./constans/index.js";
+import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from "./constants/index.js";
 
 const bootstrap = async () => {
     await initMongoConnection();
